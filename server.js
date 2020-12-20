b3a202cc670111dc00f5d0afaeeec90e715a1085
Zahraa Alshalal <you@example.com>
2020-12-19 16:34:23 -0800

    MongoClient

--- server.js
+++ server.js
@@ -16,8 +16,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
 });
 
 // routes
@@ -25,5 +26,5 @@ app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+    console.log(`App running on port ${PORT}!`);
 });
